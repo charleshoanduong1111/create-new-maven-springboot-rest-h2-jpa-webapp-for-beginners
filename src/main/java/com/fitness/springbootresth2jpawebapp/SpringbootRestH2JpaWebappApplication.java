e5981dd29280eefd867d977906ce1cd1e42fe009
Family <Family@192.168.1.160>
2023-12-18 00:08:41 +0100

    init project create-new-maven-springboot-rest-h2-jpa-webapp-for-beginners

---
+++ src/main/java/com/fitness/springbootresth2jpawebapp/SpringbootRestH2JpaWebappApplication.java
@@ -0,0 +1,13 @@
+package com.fitness.springbootresth2jpawebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestH2JpaWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestH2JpaWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitness/springbootresth2jpawebapp/SpringbootRestH2JpaWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.springbootresth2jpawebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestH2JpaWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
